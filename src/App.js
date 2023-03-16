c09731f0d05b124e37de3d7af558a2774be9a11c
sam <samscloud2@gmail.com>
2023-03-16 12:31:13 -0300

    demonstrating (userReducer) vs (useState)

--- src/App.js
+++ src/App.js
@@ -16,14 +16,18 @@ function App() {
 
   const initialState = {money: 100}; 
   const [state, dispatch] = useReducer(reducer, initialState); 
-  // const [states, setState] = useState(initialState, reducer)
+  const [state1, setState] = useState(initialState, reducer)
 
   return (
     <div className="App">
-      <h1>Wallet : {state.money}</h1>
+      {/* <h1>Wallet : {state.money}</h1> */}
+      {/* doesn't render anything because useState doesn't handle complex data types like arrays or objects */}
+      <h1>Wallet : {state1.money}</h1> 
       <div>
-        <button onClick={()=> dispatch({type : 'ride'})}>A new customer</button>
-        <button onClick={()=> dispatch({type : 'fuel'})}>Refil tank</button>
+        {/* <button onClick={()=> dispatch({type : 'ride'})}>A new customer</button>
+        <button onClick={()=> dispatch({type : 'fuel'})}>Refil tank</button> */}
+        <button onClick={()=> setState({type : 'ride'})}>A new customer</button>
+        <button onClick={()=> setState({type : 'fuel'})}>Refil tank</button>
       </div>
     </div>
   );
